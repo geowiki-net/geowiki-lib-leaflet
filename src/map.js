792d87a478230dd7c088b3feb97880a1e606cc9c
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-04-03 08:09:54 +0200

    Emit 'map-init' after initializing map

--- src/map.js
+++ src/map.js
@@ -25,6 +25,8 @@ function appInit (app, callback) {
 
   app.map.attributionControl.setPrefix('<a target="_blank" href="' + encodeURI(app.config.app.url) + '">' + app.config.app.name + '</a>')
 
+  app.emit('map-init', map)
+
   app.map.on('moveend', (e) => {
     if (app.interactive) {
       app.updateLink()
